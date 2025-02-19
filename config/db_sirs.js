970728f7f5a48abc4998caa31d217e043c0933eb
Ichwan.ElHamdhani <boootx@gmail.com>
2025-02-19 15:19:03 +0700

    db di env

--- config/db_sirs.js
+++ config/db_sirs.js
@@ -1,10 +1,11 @@
+require('dotenv').config();
 const mysql = require('mysql2');
 
 const db = mysql.createPool({
-    host: '192.168.88.75',
-    user: 'umum',
-    password: 'PenunjangDB.RSUMP',
-    database: 'rsi_pxridb',
+    host: process.env.DB_HOST_SIRS,
+    user: process.env.DB_USER_SIRS,
+    password: process.env.DB_PASSWORD_SIRS,
+    database: process.env.DB_NAME_SIRS,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
